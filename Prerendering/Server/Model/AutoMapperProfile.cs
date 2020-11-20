84b67898187d21021a90ac2e7d081e10b91bdbce
Christian Weyer <christian.weyer@thinktecture.com>
2020-11-20 21:02:33 +0100

    Adding prerendering sample

---
+++ Prerendering/Server/Model/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BlazorWasmPrerendering.Shared.DTO;
+
+namespace BlazorWasmPrerendering.Server.Model
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Conference, ConferenceOverview>();
+            CreateMap<ConferenceOverview, Conference>();
+
+            CreateMap<Conference, ConferenceDetails>();
+            CreateMap<Conference, ConferenceOverview>();
+            CreateMap<ConferenceDetails, Conference>();
+            CreateMap<ConferenceOverview, Conference>();
+        }
+    }
+}
---
+++ Prerendering/Shared/Contracts/IConferencesService.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using BlazorWasmPrerendering.Shared.DTO;
+
+namespace BlazorWasmPrerendering.Shared.Contracts
+{
+    [ServiceContract]
+    public interface IConferencesService
+    {
+        Task<IEnumerable<ConferenceOverview>> ListConferencesAsync();
+        Task<ConferenceDetails> GetConferenceDetailsAsync(ConferenceDetailsRequest request);
+        Task<ConferenceDetails> AddNewConferenceAsync(ConferenceDetails conference);
+    }
+}
