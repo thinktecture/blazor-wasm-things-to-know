f6ce37e1c58880ea663dc317008d69084ee26a7c
Christian Weyer <christian.weyer@thinktecture.com>
2020-11-23 15:56:26 +0100

    Adding Isolated JS sample

---
+++ IsolatedJS/Client/Webcam/WebcamOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorLibs.Webcam
+{
+    public class WebcamOptions
+    {
+        public int Width { get; set; }
+        public string VideoID { get; set; }
+        public string CanvasID { get; set; }
+        public string PhotoID { get; set; }
+    }
+}
