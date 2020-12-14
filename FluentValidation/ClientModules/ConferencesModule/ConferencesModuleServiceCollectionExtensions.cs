c6262240f7768b0d483fc7b8c6b25f6b12301fa0
Christian Weyer <christian.weyer@thinktecture.com>
2020-12-14 15:23:02 +0100

    Re-adding FluentValidation sample

---
+++ FluentValidation/ClientModules/ConferencesModule/ConferencesModuleServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Blazored.Toast;
+using ConfTool.ClientModules.Conferences.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConfTool.ClientModules.Conferences
+{
+    public static class ConferencesModuleServiceCollectionExtensions
+    {
+        public static IServiceCollection AddConferencesModule(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<IConferencesServiceClient, ConferencesServiceClient>();
+            services.AddScoped<CountriesServiceClient>();
+
+            services.AddHttpClient("Conferences.ServerAPI", client =>
+                client.BaseAddress = new Uri(config[Configuration.BackendUrlKey]));
+
+            services.AddDialog();
+
+            services.AddBlazoredToast();
+
+            return services;
+        }
+    }
+}
