11a341479441ecb0bba4246755052b1f9f4e5e1a
Christian Weyer <christian.weyer@thinktecture.com>
2020-12-26 14:32:43 +0100

    Fix memory cleanup

--- VideoPlayer/Client/wwwroot/videoServiceUnmarshaledJSInterop.js
+++ VideoPlayer/Client/wwwroot/videoServiceUnmarshaledJSInterop.js
@@ -1,4 +1,6 @@
 ﻿window.videoServiceUnmarshaledReference = () => {
+    var videoUrl;
+
     return {
         createObjectURL: function (data, name, type) {
             const content = Blazor.platform.toUint8Array(data);
@@ -6,10 +8,12 @@
             const contentType = BINDING.conv_string(type);
 
             const file = new File([content], fileName, { type: contentType });
+            videoUrl = URL.createObjectURL(file);
 
-            return BINDING.js_to_mono_obj(URL.createObjectURL(file));
+            return BINDING.js_to_mono_obj(videoUrl);
         },
         dispose: function () {
+            URL.revokeObjectURL(videoUrl);
             DotNet.disposeJSObjectReference(this);
         }
     };
